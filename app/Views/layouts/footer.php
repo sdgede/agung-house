791a695004bc175ba9ccacfe0ffc8ba73da5cab8
sdgede <gede.indrawan49@smk.belajar.id>
2024-10-28 16:40:08 +0800

    layouts

---
+++ app/Views/layouts/footer.php
@@ -0,0 +1,4 @@
+
+<script src="<?= base_url('assets/js/bootstrap.bundle.js'); ?>"></script> 
+</body>
+</html>
---
+++ app/Views/layouts/template.php
@@ -0,0 +1,8 @@
+<?php 
+include 'header.php'; ?>
+
+<main role="main" class="col-md-9 mx-auto col-lg-10 px-md-4">
+    <?= $this->renderSection('content'); ?>
+</main>
+ 
+<?php include 'footer.php'; ?>
