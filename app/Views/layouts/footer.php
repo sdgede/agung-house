a7da5359773c6c410fcaa4865388578a3c9a7560
SciverZx <anandaharysmk@gmail.com>
2024-10-29 12:01:52 +0800

    layouts

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -1,4 +1,5 @@
 
 <script src="<?= base_url('assets/js/bootstrap.bundle.js'); ?>"></script> 
+<script src="<?= base_url('AOS/dist/aos.js'); ?>"></script> 
 </body>
 </html>
--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -4,7 +4,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('AOS/dist/aos.js'); ?>">
 </head>
 <body>
- 
+
 <?php include 'navbar.php' ?>
