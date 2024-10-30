aa40120acb214fca1155f08f06a005887136e73a
sdgede <gede.indrawan49@smk.belajar.id>
2024-10-30 15:55:12 +0800

    selasa

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -1,5 +1,14 @@
 
 <script src="<?= base_url('assets/js/bootstrap.bundle.js'); ?>"></script> 
-<script src="<?= base_url('AOS/dist/aos.js'); ?>"></script> 
+<script src="<?= base_url('AOS/src/js/aos.js'); ?>"></script> 
+<script>
+  $(document).on('ready', function () {
+    // initialization of aos
+    AOS.init({
+      duration: 650,
+      once: true
+    });
+  });
+</script>
 </body>
 </html>
