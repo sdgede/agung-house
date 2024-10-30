1d167549350cede3d253d3174c2c15f879348a92
SciverZx <anandaharysmk@gmail.com>
2024-10-30 10:42:25 +0800

    room_detail

--- app/Views/content/room_detail.php
+++ app/Views/content/room_detail.php
@@ -5,35 +5,48 @@
         <div class="col-lg-7 p-1 pt-0">
             <div class="card-image">
                 <img class="rooms-image" src="<?= base_url('assets/img/rooms-left.png'); ?>" alt="">
-                <button class="button-show-all-photos">
-                    <i class="fas fa-image"></i>
-                </button>
+                <a href="#" class="button-show-all-photos">
+                    <i class="fas fa-image rooms-icon"></i>
+                    <p class="m-0">Show all photos</p>
+                </a>
             </div>
         </div>
         <div class="col-lg-5">
             <div class="row rooms-wrap">
-                <div class="col-lg-6 p-1 pt-0">
-                    <div class="card-image">
-                        <img class="rooms-image" src="<?= base_url('assets/img/rooms-rigth1.png'); ?>" alt="">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-sm-6 p-1 pt-0">
+                            <div class="card-image">
+                                <img class="rooms-image" src="<?= base_url('assets/img/rooms-rigth1.png'); ?>" alt="">
+                            </div>
+                        </div>
+                        <div class="col-sm-6 p-1 pt-0">
+                            <div class="card-image">
+                                <img class="rooms-image" src="<?= base_url('assets/img/rooms-rigth2.png'); ?>" alt="">
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="col-lg-6 p-1 pt-0">
-                    <div class="card-image">
-                        <img class="rooms-image" src="<?= base_url('assets/img/rooms-rigth2.png'); ?>" alt="">
-                    </div>
-                </div>
-                <div class="col-lg-6 p-1">
-                    <div class="card-image">
-                        <img class="rooms-image" src="<?= base_url('assets/img/rooms-rigth2.png'); ?>" alt="">
-                    </div>
-                </div>
-                <div class="col-lg-6 p-1">
-                    <div class="card-image">
-                        <img class="rooms-image" src="<?= base_url('assets/img/rooms-rigth3.png'); ?>" alt="">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-sm-6 p-1">
+                            <div class="card-image">
+                                <img class="rooms-image" src="<?= base_url('assets/img/rooms-rigth2.png'); ?>" alt="">
+                            </div>
+                        </div>
+                        <div class="col-sm-6 p-1">
+                            <div class="card-image">
+                                <img class="rooms-image" src="<?= base_url('assets/img/rooms-rigth3.png'); ?>" alt="">
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+        <a href="#" class="button-show-all-photos2">
+            <i class="fas fa-image rooms-icon"></i>
+            <p class="m-0">Show all photos</p>
+        </a>
     </div>
 </div>
 <?= $this->EndSection(); ?>
