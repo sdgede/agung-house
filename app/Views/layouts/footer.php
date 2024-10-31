b0f62d507113b1696c65d3fad729d70edc513f78
sdgede <gede.indrawan49@smk.belajar.id>
2024-10-31 17:03:16 +0800

    rabu

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -1,5 +1,6 @@
 
 <script src="<?= base_url('assets/js/bootstrap.bundle.js'); ?>"></script> 
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="<?= base_url('AOS/src/js/aos.js'); ?>"></script> 
 <script>
   $(document).on('ready', function () {
