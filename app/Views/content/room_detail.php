283d114cab934876c69be7a2e83a89b1d65364d2
SciverZx <anandaharysmk@gmail.com>
2024-10-31 12:07:13 +0800

    room_detail

--- app/Views/content/room_detail.php
+++ app/Views/content/room_detail.php
@@ -45,10 +45,6 @@
                     </div>
                 </div>
             </div>
-            <a href="#" class="button-show-all-photos2">
-                <i class="fas fa-image rooms-icon"></i>
-                <p class="m-0">Show all photos</p>
-            </a>
         </div>
     </section>
     <!-- End Room Image -->
@@ -57,6 +53,7 @@
     <section class="rooms-main">
         <div class="row rooms-main-row">
             <div class="col-lg-7">
+                <!-- Room Information -->
                 <div class="card">
                     <div class="card-header">
                         <div class="room-bedge">
@@ -122,7 +119,53 @@
                         </div>
                     </div>
                 </div>
+                <!-- Stay Information -->
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="stay-info-title m-0">Stay Information</h5>
+                    </div>
+                    <div class="card-body stay-info-body">
+                        Providing lake views, The Symphony 9 Tam Coc in Ninh Binh provides accommodation, an outdoor swimming pool, a bar, a shared lounge, a garden and barbecue facilities. Complimentary WiFi is provided.
+                        <br>
+                        <br>
+                        There is a private bathroom with bidet in all units, along with a hairdryer and free toiletries.
+                        <br>
+                        <br>
+                        The Symphony 9 Tam Coc offers a terrace.
+                        <br>
+                        <br>
+                        Both a bicycle rental service and a car rental service are available at the accommodation, while cycling can be enjoyed nearby.
+                    </div>
+                    <div class="card-footer"></div>
+                </div>
+
+                <!-- Amenities -->
+                <div class="card">
+                    <div class="card-header amenities-header">
+                        <h5 class="stay-info-title m-0">Amenities</h5>
+                        <p class="m-0">About the property's amenities and services</p>
+                    </div>
+                    <div class="card-body stay-info-body">
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <i class="fas fa-wifi"></i>
+                                Wifi
+                            </div>
+                            <div class="col-lg-4">
+                                <i class="fas fa-tv"></i>
+                                Televition
+                            </div>
+                            <div class="col-lg-4">
+                                <i class="far fa-air-conditioner"></i>
+                                Air Conditioner
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer"></div>
+                </div>
             </div>
+
+            <!-- Col - 4 -->
             <div class="col-lg-4">
                 <div class="card">
                     <div class="card-header">
@@ -152,52 +195,56 @@
                                     </div>
                                 </div>
                                 <hr>
-                                <div class="col-lg-12">
+                                <div class="col-lg-6">
                                     <i class="fas fa-map-marker-alt"></i>
                                     <div class="guests">
                                         <span>Guests</span>
                                         <h5 class="m-0">3 Guests</h5>
                                     </div>
                                 </div>
+                                <div class="col-lg-6">
+                                </div>
                             </div>
                         </div>
-                        <hr>
+                        <div class="total-price">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="service">
+                                        $200 x 3 night
+                                    </div>
+                                    <div class="price">
+                                        $600
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="service">
+                                        Service charge
+                                    </div>
+                                    <div class="price">
+                                        $0
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr class="m-0">
+                        <div class="total-prices">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="service">
+                                        Total
+                                    </div>
+                                    <div class="price">
+                                        $600
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div class="card-footer">
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <div class="room-info">
-                                            <i class="fas fa-user"></i>
-                                            2 Guests
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <div class="room-info">
-                                            <i class="fas fa-bed"></i>
-                                            1 Beds
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <div class="room-info">
-                                            <i class="fas fa-bath"></i>
-                                            1 Baths
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <div class="room-info">
-                                            <i class="fas fa-door-open"></i>
-                                            1 Bedrooms
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <a href="https://wa.me/6282147836034?test=Booking%20Deluxe%20Room" class="booking-wa-button">
+                            <i class="fab fa-whatsapp"></i>
+                            Book Now
+                        </a>
                     </div>
                 </div>
             </div>
--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -6,7 +6,8 @@
     <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css') ?>">
     <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> -->
+    <link rel="stylesheet" href="<?= base_url('Font-Awesome/css/all.css') ?>">
 </head>
 <body>
 
